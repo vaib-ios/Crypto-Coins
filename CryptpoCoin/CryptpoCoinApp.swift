cfd314c2c62a126704703afbffea509b1ccf334e
vaibhav <vaibhavatios@gmail.com>
2025-12-17 02:16:37 +0530

    Initial Commit

---
+++ CryptpoCoin/CryptpoCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptpoCoinApp.swift
+//  CryptpoCoin
+//
+//  Created by Vaibhav Limbani on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptpoCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
